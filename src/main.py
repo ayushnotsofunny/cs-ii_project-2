529dcb51924c2c5e26ec175980b7e4633a741315
ayushnotsofunny <aadhikari3@caldwell.edu>
2026-05-02 17:37:29 +0000

    May 2, 2026, 1:37 PM

--- src/main.py
+++ src/main.py
@@ -7,6 +7,15 @@ from datetime import date
 
 APP_PATH = os.patj.dirname(os.path.abspath(__file__))
 
+def get_data_path(filename: str) -> star:
+    """ returns the paths to an assest files givpen the filename """
+    return os.path.join(APP_PATH, "data", filename)
+
+
+# load data
+data_files = get_data_path("expenses.csv")
+if os.path.exists(data_file):
+    df = pd.read_csv(data_file
 
 
 
